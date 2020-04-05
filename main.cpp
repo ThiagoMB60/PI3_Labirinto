6ae9fc22e3d6ec0bcceea97d3b7c6312afef0e5d
Thiago <thiagomoreira60@gmail.com>
2020-04-05 13:42:51 -0300

    bug do movimento resolvido

--- main.cpp
+++ main.cpp
@@ -1,25 +1,61 @@
 #include <iostream>
 #include <windows.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
 
 using namespace std;
 
 struct matrizGame
 {
-    char caractere;
+    char caractere;  //0 = ' '; 1 = 'X'; 2  = 206; 3 = 220;
+    int indice = 0;
 };
-
 const int tm1 = 21, tm2 = 26, tm3 = 31, tm4 = 36, tm5 = 41, tmax = tm5;
 
+int posLinha;
+int posColuna;
+int posicaoI = 40, posicaoJ = 21;
 matrizGame matriz5[tmax][tmax];
 matrizGame matriz4[tmax][tmax];
 matrizGame matriz3[tmax][tmax];
 matrizGame matriz2[tmax][tmax];
 matrizGame matriz1[tmax][tmax];
 
-int posLinha;
-int posColuna;
+void cabecalho(int tamanho);
+void rodape(int tamanho);
+void bandeira(matrizGame marMax[][tmax],int tamanho);
+void printMatriz(matrizGame matMax[][tmax], int tamanho);
+void tela5(matrizGame matriz5[][tmax]);
+void tela4(matrizGame matriz4[][tmax]);
+void tela3(matrizGame matriz3[][tmax]);
+void tela2(matrizGame matriz2[][tmax]);
+void tela1(matrizGame matriz1[][tmax]);
+void alteraPosicao(bool subir,bool descer, bool direita, bool esquerda);
+void selectMatriz(int matriz);
+void movimento();
 
-void cabecalho(int tamanho){
+int main()
+{
+    /*keybd_event(VK_MENU,0x36,0,0);
+    keybd_event(VK_RETURN,0x1C,0,0);
+    keybd_event(VK_RETURN,0x1C,KEYEVENTF_KEYUP,0);
+    keybd_event(VK_MENU,0x38,KEYEVENTF_KEYUP,0);*/
+
+    int resposta = 5;
+    selectMatriz(resposta);
+
+    do
+    {
+        movimento();
+    }
+    while(resposta !=0);
+}
+
+void cabecalho(int tamanho)
+{
     cout << "\n\n\n\t\t      ";
     for (int i = 0 ; i < tamanho; i++)
     {
@@ -29,7 +65,9 @@ void cabecalho(int tamanho){
     }
     cout << "\n";
 }
-void rodape(int tamanho){
+
+void rodape(int tamanho)
+{
     cout << "\n\n\t\t      ";
     for (int i = 0 ; i < tamanho; i++)
     {
@@ -39,22 +77,26 @@ void rodape(int tamanho){
     }
 }
 
-void bandeira(matrizGame matMax[][tmax], int tamanho){
+void bandeira(matrizGame matMax[][tmax], int tamanho)
+{
     for(int linha = 0; linha < tamanho; linha++)
     {
         for(int coluna = 0; coluna < tamanho; coluna++)
         {
             if ((matMax[linha][coluna].caractere == ' ') && ((matMax[linha][coluna+1].caractere == ' ' &&
-                (matMax[linha-1][coluna].caractere == ' ' || matMax[linha+1][coluna].caractere == ' ')) ||
-                (matMax[linha][coluna-1].caractere == ' ' && (matMax[linha-1][coluna].caractere == ' ' ||
-                matMax[linha+1][coluna].caractere == ' ')))){
-                    matMax[linha][coluna].caractere = 220;
-                }
+                    (matMax[linha-1][coluna].caractere == ' ' || matMax[linha+1][coluna].caractere == ' ')) ||
+                    (matMax[linha][coluna-1].caractere == ' ' && (matMax[linha-1][coluna].caractere == ' ' ||
+                            matMax[linha+1][coluna].caractere == ' '))))
+            {
+                matMax[linha][coluna].caractere = 220;
+                matMax[linha][coluna].indice = 3;
+            }
         }
     }
 }
 
-void printMatriz(matrizGame matMax[][tmax], int tamanho){
+void printMatriz(matrizGame matMax[][tmax], int tamanho)
+{
     cabecalho(tamanho);
     for(int linha = 0; linha < tamanho; linha++)
     {
@@ -72,8 +114,49 @@ void printMatriz(matrizGame matMax[][tmax], int tamanho){
     rodape(tamanho);
 }
 
+void alteraPosicao(bool subir,bool descer, bool direita, bool esquerda)
+{
+    if(subir == true)
+    {
+        while((matriz5[posicaoI][posicaoJ].indice != 3)){
+            if((matriz5[posicaoI-1][posicaoJ].indice == 1)){
+                break;
+            }
+            posicaoI--;
+        }
+    }
+    else if(descer == true)
+    {
+        while((matriz5[posicaoI][posicaoJ].indice != 3)){
+            if((matriz5[posicaoI+1][posicaoJ].indice == 1)){
+                break;
+            }
+            posicaoI++;
+        }
+    }
+    else if(direita == true)
+    {
+        while((matriz5[posicaoI][posicaoJ].indice != 3)){
+            if((matriz5[posicaoI][posicaoJ+1].indice == 1)){
+                break;
+            }
+            posicaoJ++;
+        }
+    }
+    else if(esquerda == true)
+    {
+        while((matriz5[posicaoI][posicaoJ].indice != 3)){
+            if((matriz5[posicaoI][posicaoJ-1].indice == 1)){
+                break;
+            }
+            posicaoJ--;
+        }
+    }
+}
+
 void tela5(matrizGame matriz5[][tmax])
 {
+
     for(int linha = 0; linha < tm5; linha++)
     {
         for(int coluna = 0; coluna < tm5; coluna++)
@@ -86,10 +169,11 @@ void tela5(matrizGame matriz5[][tmax])
                     (linha == tm1 && coluna > 10 && coluna <= tm1)||(linha == 30 && coluna >= 10 && coluna <= 16)||
                     (linha == 34 && coluna < 16 && coluna > 4)||(linha == 15 && coluna > 5 && coluna <29)||
                     (linha == 37 && coluna > 28 && coluna < tm4)||(linha == 10 && coluna < tm4 && coluna > 10)||
-                    (linha == 5 && coluna > 11 && coluna < 22))  //
+                    (linha == 5 && coluna > 11 && coluna < 22))
             {
 
                 matriz5[linha][coluna].caractere = ' ';
+                matriz5[linha][coluna].indice = 0;
             }
 
             //rotas alternativas
@@ -107,19 +191,25 @@ void tela5(matrizGame matriz5[][tmax])
                     (linha == 39 && coluna > 0 && coluna < 21) || (linha == 21 && coluna > 0 && coluna < 4) || (coluna == 1 && linha < 39 && linha > 21) ||
                     (coluna == 3 && linha < 21 && linha > 10) || (linha == 27 && coluna > 11 && coluna < 20) || (linha == 19 && coluna > 11 && coluna < 25) ||
                     (linha == 23 && coluna > 14 && coluna < 25) || (coluna == 12 && linha < 27 && linha > 19) || (coluna == 24 && linha < 23 && linha > 19) )
-                    {
+            {
                 matriz5[linha][coluna].caractere = ' ';
+                matriz5[linha][coluna].indice = 0;
             }
             else
             {
                 matriz5[linha][coluna].caractere = 'X';
+                matriz5[linha][coluna].indice = 1;
             }
-
         }
     }
+
+    matriz5[posicaoI][posicaoJ].caractere = 206;
+    matriz5[posicaoI][posicaoJ].indice = 2;
     bandeira(matriz5,tm5);
     printMatriz(matriz5,tm5);
+    Sleep(1000);
 }
+
 void tela4(matrizGame matriz4[][tmax])
 {
     for(int linha = 0; linha < tm4; linha++)
@@ -143,6 +233,7 @@ void tela4(matrizGame matriz4[][tmax])
     bandeira(matriz4,tm4);
     printMatriz(matriz4,tm4);
 }
+
 void tela3(matrizGame matriz3[][tmax])
 {
     for(int linha = 0; linha < tm3; linha++)
@@ -166,6 +257,7 @@ void tela3(matrizGame matriz3[][tmax])
     bandeira(matriz4,tm4);
     printMatriz(matriz3,tm3);
 }
+
 void tela2(matrizGame matriz2[][tmax])
 {
     for(int linha = 0; linha < tm2; linha++)
@@ -189,6 +281,7 @@ void tela2(matrizGame matriz2[][tmax])
     bandeira(matriz4,tm4);
     printMatriz(matriz2,tm2);
 }
+
 void tela1(matrizGame matriz1[][tmax])
 {
     for(int linha = 0; linha < tm1; linha++)
@@ -235,25 +328,45 @@ void selectMatriz(int matriz)
     }
 }
 
-int main()
+void movimento()
 {
+    char direcao[40];
 
-    keybd_event(VK_MENU,0x36,0,0);
-    keybd_event(VK_RETURN,0x1C,0,0);
-    keybd_event(VK_RETURN,0x1C,KEYEVENTF_KEYUP,0);
-    keybd_event(VK_MENU,0x38,KEYEVENTF_KEYUP,0);
+    cout << "\n\nPara qual direcao deseja ir? ";
+    fflush(stdin);
+    gets(direcao);
 
-    int resposta;
-
-    do
+    for(int i = 0; i < strlen(direcao); i++)
     {
-        cout << "\n\n\tQual matriz deseja imprimir? (0 para sair).\n\n\t";
-        cin >> resposta;
-        selectMatriz(resposta);
-        cout << "\n\n\n";
-        system ("pause");
-        system ("cls");
-
+        if (direcao[i] == 119)      //para cima
+        {
+            system("CLS");
+            alteraPosicao(1,0,0,0);
+            tela5(matriz5);
+        }
+        else if (direcao[i] == 100)  //para direita
+        {
+            system("CLS");
+            alteraPosicao(0,0,1,0);
+            tela5(matriz5);
+        }
+        else if (direcao[i] == 115) //para baixo
+        {
+            system("CLS");
+            alteraPosicao(0,1,0,0);
+            tela5(matriz5);
+        }
+        else if (direcao[i] == 97)   //para esqueda
+        {
+            system("CLS");
+            alteraPosicao(0,0,0,1);
+            tela5(matriz5);
+        }
+        else
+        {
+            system("CLS");
+            alteraPosicao(0,0,0,0);
+            tela5(matriz5);
+        }
     }
-    while(resposta !=0);
 }
